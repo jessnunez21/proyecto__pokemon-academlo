1aa0cd58814786acb47049a2a0b70aba792d4d1c
jessnunez21 <nunezjess85@gmail.com>
2023-06-22 23:29:45 -0300

    'actualizacion_error'

--- src/components/error/ErrorInput.jsx
+++ src/components/error/ErrorInput.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
+import imgError from '/errorImg/pokeImg.jpg'
 
 const ErrorInput = ({name}) => {
 
   
 
   return (
-    <div className='cart__error'>
-        <h2>The pokemon "<span>{name}</span>" doesn't exist</h2>
+    <div className='cart__error' >
+      <div><img  className='img__error' src={imgError} alt="imagen-error" /></div>
+        <h2 className='error'>The pokemon "<span className='name__error'>{name}</span>" doesn't exist</h2>
     </div>
   )
 }
